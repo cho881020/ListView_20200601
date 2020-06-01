10982e31c5778401babaddf1da57e2e4f109c1d4
kj_cho <kj_cho@idealidea.co.kr>
2020-06-01 20:46:01 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/tjoeun/listview_20200601/BaseActivity.kt
@@ -0,0 +1,12 @@
+package kr.co.tjoeun.listview_20200601
+
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity() {
+
+    val mContext = this
+
+    abstract fun setupEvents()
+    abstract fun setValues()
+
+}
